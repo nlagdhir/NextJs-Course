54c82ab84c26cf5493e0ca192ca32ac220a30509
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-09-19 22:05:11 +0530

    created homepage, event page and event details page

---
+++ 2.working-with-file-based-routing/pages/events/[...slug].js
@@ -0,0 +1,14 @@
+import Router from "next/router";
+
+
+const FilteredEventPage = () => {
+  const router = Router();
+  console.log(router);
+  return (
+    <div>
+      <h1>Filtered Event Page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventPage;
